1881a52049dcb49ee0b8aa70ca76c91ccee45ced
Patrick Tonne <happy.camper.git@gmail.com>
2019-07-30 00:41:14 -0400

    added verifyUrl(); updated try/catch logic; addded count variable to goToSectionAndGetDom() to handle errors;

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const stream = require('stream');
 const secrets = require('../config/secrets.json');
 
 
-const configureAws = async() => {
+const configureAws = async () => {
     aws.config.setPromisesDependency();
     aws.config.update({
         accessKeyId: secrets.aws.access_key_ID,
@@ -15,30 +15,31 @@ const configureAws = async() => {
     });
 };
 
-const restart = async(browser) => {
+const restart = async (browser) => {
     browser.close();
     await timeout(getRandomArbitrary(3000, 4000));
     await exports.start();
 };
 
-const logIn = async(browser, page) => {
-        try {
-            const userNameHandle = await page.$('#username');
-            const passWordHandle = await page.$('#password');
-            await userNameHandle.type(secrets.linkedin.username, {
-                delay: 10
-            });
-            await passWordHandle.type(secrets.linkedin.password, {
-                delay: 10
-            });
-            await Promise.all([page.waitForNavigation(),
-                page.click('button.btn__primary--large')
-        ])} catch (err) {
-            await restart(browser);
-        }
+const logIn = async (browser, page) => {
+    try {
+        const userNameHandle = await page.$('#username');
+        const passWordHandle = await page.$('#password');
+        await userNameHandle.type(secrets.linkedin.username, {
+            delay: 10
+        });
+        await passWordHandle.type(secrets.linkedin.password, {
+            delay: 10
+        });
+        await Promise.all([page.waitForNavigation(),
+            page.click('button.btn__primary--large')
+        ])
+    } catch (err) {
+        await restart(browser);
+    }
 };
 
-const goToLinkedIn = async(page) => {
+const goToLinkedIn = async (page) => {
     try {
         await Promise.all([page.waitForNavigation('domcontentloaded'),
             page.goto('https://www.linkedin.com/login', 'domcontentloaded')
@@ -48,36 +49,44 @@ const goToLinkedIn = async(page) => {
     }
 };
 
-const goToSiteSection = async(browser, page, url) => {
-        try {
-            await Promise.all([page.goto(url, 'domcontentloaded'),
-        ]);
-            if (page.url() !== url) {
-                await timeout(getRandomArbitrary(3000, 4000));
-                await page.evaluate(() => {
-                    const homeNavButton = $('.org-page-navigation__item-anchor:contains("Home")');
-                    homeNavButton.click();
-                });
-            }
-            await setFeedToMostRecent(page);
-            await Promise.all([page.waitFor(getRandomArbitrary(6000, 10000)),
-            scroll(page)
-        ])
-        } catch (err) {
-            console.log(err);
-            await restart(browser)
-        }
+const verifyUrl = async (page, url) => {
+    if (url === page.url()) {
+        return
+    }
+    const parsedUrl = await new URL(page.url());
+    if (parsedUrl.pathname === '/feed' || parsedUrl.pathname === '/feed/') {
+        throw Error('Url led to personal feed')
+    } else {
+        await timeout(getRandomArbitrary(3000, 4000));
+        await page.evaluate(() => {
+            const homeNavButton = $('.org-page-navigation__item-anchor:contains("Home")');
+            homeNavButton.click();
+        });
+    }
 };
 
-const setFeedToMostRecent = async(page) => {
+const goToSiteSection = async (browser, page, url) => {
+    try {
+        await Promise.all([page.goto(url, 'domcontentloaded'), ]);
+        await verifyUrl(page, url);
+        await setFeedToMostRecent(page);
+        await Promise.all([page.waitFor(getRandomArbitrary(6000, 10000)),
+            scroll(page)
+        ])
+    } catch (err) {
+        console.log(err);
+        throw err
+    }
+};
+
+const setFeedToMostRecent = async (page) => {
     await timeout(getRandomArbitrary(3000, 4000));
     try {
         await Promise.all([page.waitForSelector('.sort-dropdown__icon'),
-        page.click('.sort-dropdown__icon')
-    ])
-    } catch(e) {
-        await timeout(getRandomArbitrary(4000, 5000));
-        await setFeedToMostRecent(page)
+            page.click('.sort-dropdown__icon')
+        ])
+    } catch (e) {
+        throw Error('issue setting feed to most recent');
     }
     await page.evaluate(() => {
         $('.sort-dropdown__list-item-button:contains(Recent)').click();
@@ -85,7 +94,7 @@ const setFeedToMostRecent = async(page) => {
     await timeout(getRandomArbitrary(2000, 3000))
 };
 
-const getDomAndUpload = async(page) => {
+const getDomAndUpload = async (page) => {
     const content = await page.content();
     const date = Date.now();
     const name = await getName(page);
@@ -102,34 +111,43 @@ const getDomAndUpload = async(page) => {
     });
 };
 
-const goToSectionAndGetDom = async(browser, page, urls) => {
-    for (let i = 0; i<urls.length; ++i) {
-        let url = urls[i];
-        await goToSiteSection(browser, page, url);
+const goToSectionAndGetDom = async (browser, page, urls, count=0) => {
+    for (let i = count; i < urls.length; ++count) {
+        console.log(count);
+        let url = urls[count];
+        try {
+            await goToSiteSection(browser, page, url);
+        } catch (err) {
+            console.log(err);
+            console.log('it reached the top boi');
+            await goToSectionAndGetDom(browser, page, urls, ++count)
+            // let url = urls[i + 1];
+            // await goToSiteSection(browser, page, url, i)
+        }
         await getDomAndUpload(page);
     }
 };
 
-const scroll = async(page) => {
+const scroll = async (page) => {
     try {
         await page.evaluate(() => {
             const scroll = () => {
-            let postDates = $('#organization-feed').find('.feed-shared-actor__sub-description');
-            let postDateLength = postDates.length;
-            if (postDateLength !== 0) {
-                let lastEntryDate = postDates[postDateLength-1].textContent.trim();
-                const timeRegex = /(\d+)(m|h|d)/;
-                let match = lastEntryDate.match(timeRegex);
-                if (!match){
-                    return;
+                let postDates = $('#organization-feed').find('.feed-shared-actor__sub-description');
+                let postDateLength = postDates.length;
+                if (postDateLength !== 0) {
+                    let lastEntryDate = postDates[postDateLength - 1].textContent.trim();
+                    const timeRegex = /(\d+)(m|h|d)/;
+                    let match = lastEntryDate.match(timeRegex);
+                    if (!match) {
+                        return;
+                    }
+                    window.scrollTo(0, document.body.scrollHeight);
+                    setTimeout(scroll, 2000)
                 }
-                window.scrollTo(0, document.body.scrollHeight);
-                setTimeout(scroll, 2000)
-            }
-        };
-        scroll();
+            };
+            scroll();
         });
-    } catch(e) {
+    } catch (e) {
         console.log(e);
     }
 };
@@ -142,14 +160,14 @@ const getRandomArbitrary = (min, max) => {
     return Math.random() * (max - min) + min;
 };
 
-const getName = async(page) => {
+const getName = async (page) => {
     return page.evaluate(() => {
         let rawText = document.querySelector('.org-top-card-summary__title').textContent.trim();
         return rawText.split(' ').join('_')
     });
 };
 
-const scrapeUrls = async(browser, page, path) => {
+const scrapeUrls = async (browser, page, path) => {
     const instream = await fs.createReadStream(path);
     const outstream = await new stream;
     const rl = await readline.createInterface(instream, outstream);
@@ -165,7 +183,7 @@ const scrapeUrls = async(browser, page, path) => {
     });
 };
 
-exports.start = async() => {
+exports.start = async () => {
     await configureAws();
     const browser = await puppeteer.launch({
         headless: false
@@ -174,7 +192,7 @@ exports.start = async() => {
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
     await goToLinkedIn(page);
     await logIn(browser, page);
-    await scrapeUrls(browser, page,'urls.txt');
+    await scrapeUrls(browser, page, 'urls.txt');
 };
 
 exports.start();
